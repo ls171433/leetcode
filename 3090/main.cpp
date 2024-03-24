c7138398de7971f2d90ca2490d05b605ae067a44
ls171433 <lslslsls27@hotmail.com>
2024-03-24 16:28:32 +0800

    add 3090 3093

---
+++ 3090/main.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include<iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maximumLengthSubstring(string s)
+    {
+        int current[26] = {};
+        int max = 0;
+        for(size_t i = 0, j = 0; i < s.size(); ++i)
+        {
+            char ci = s[i];
+            int& numi = (current[ci - 'a']);
+            ++numi;
+            while (numi >= 3)
+            {
+                char cj = s[j];\
+                int& numj = (current[cj - 'a']);
+                --numj;
+                ++j;
+            }
+            int newmax = i - j + 1;
+            if (newmax > max)
+            {
+                max = newmax;
+            }
+        }
+        return max;
+    }
+};
+
+int main()
+{
+    string s = "bcbbbcba";
+    auto result = Solution().maximumLengthSubstring(s);
+    cout << result << endl;
+}
