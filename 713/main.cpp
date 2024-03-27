4dd8ad086e9521164b4248fdbb95c3189cb51fa6
ls171433 <lslslsls27@hotmail.com>
2024-03-27 22:08:09 +0800

    add 713

---
+++ 713/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int product = 1;
+        int result = 0;
+        for (size_t i = 0, j = 0; i < nums.size(); ++i)
+        {
+            product *= nums[i];
+            while (product >= k && i >= j)
+            {
+                product /= nums[j];
+                ++j;
+            }
+            result += i - j + 1;
+        }
+        return result;
+    }
+};
