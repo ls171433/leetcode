fc42084f3a2a8b3006d61af82c80e337732a66ce
ls171433 <lslslsls27@hotmail.com>
2025-07-27 14:24:37 +0800

    update 1717

--- src/1717.cpp
+++ src/1717.cpp
@@ -8,26 +8,21 @@ class Solution
 public:
     int maximumGain(string s, int x, int y)
     {
+        char a = 'a', b = 'b';
+        if (x < y)
+        {
+            swap(b, a);
+            swap(x, y);
+        }
         int count_1_a = 0, count_1_b = 0;
-        int count_2_a = 0, count_2_b = 0;
-        int score1 = 0, score2 = 0;
-        int result = 0;
+        int score1 = 0;
         for (char c : s)
         {
-            if (c == 'a')
+            if (c == a)
             {
                 ++count_1_a;
-                if (count_2_b)
-                {
-                    --count_2_b;
-                    score2 += y;
-                }
-                else
-                {
-                    ++count_2_a;
-                }
             }
-            else if (c == 'b')
+            else if (c == b)
             {
                 if (count_1_a)
                 {
@@ -38,28 +33,17 @@ public:
                 {
                     ++count_1_b;
                 }
-                ++count_2_b;
             }
             else
             {
                 score1 += min(count_1_a, count_1_b) * y;
-                score2 += min(count_2_a, count_2_b) * x;
-
-                result += max(score1, score2);
 
                 count_1_a = 0;
                 count_1_b = 0;
-                count_2_a = 0;
-                count_2_b = 0;
-                score1 = 0;
-                score2 = 0;
             }
         }
         score1 += min(count_1_a, count_1_b) * y;
-        score2 += min(count_2_a, count_2_b) * x;
 
-        result += max(score1, score2);
-
-        return result;
+        return score1;
     }
 };
