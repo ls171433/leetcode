4a18a7018df71fc31ed801524ea44c57c6891754
ls171433 <lslslsls27@hotmail.com>
2025-07-31 09:50:17 +0800

    add 898

---
+++ src/898.cpp
@@ -0,0 +1,32 @@
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int subarrayBitwiseORs(vector<int> &arr)
+    {
+        unordered_set<int> result;
+        unordered_set<int> current;
+        int prev = 0;
+
+        for (int num : arr)
+        {
+            unordered_set<int> new_current;
+            new_current.insert(num);
+            for (int prev_num : current)
+            {
+                new_current.insert(prev_num | num);
+            }
+            current.swap(new_current);
+            for (int new_num : current)
+            {
+                result.insert(new_num);
+            }
+        }
+
+        return result.size();
+    }
+};
