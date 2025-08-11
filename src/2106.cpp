e88e37e9d41f8c3e5c6856205b3ef0e07355d725
ls171433 <lslslsls27@hotmail.com>
2025-08-11 10:26:04 +0800

    update 2106

--- src/2106.cpp
+++ src/2106.cpp
@@ -7,36 +7,45 @@ class Solution
 public:
     int maxTotalFruits(vector<vector<int>> &fruits, int startPos, int k)
     {
-        vector<int> new_fruits(2 * k + 1, 0);
-        for (auto &fruit : fruits)
+        int current_sum = 0;
+        int max_sum = 0;
+
+        int i = 0;
+        while (i < fruits.size() && fruits[i][0] < startPos - k)
         {
-            int position = fruit[0];
-            int amount = fruit[1];
-            if (position >= startPos - k && position <= startPos + k)
+            ++i;
+        }
+
+        int j = i;
+        while (j < fruits.size() &&
+               fruits[j][0] <= startPos + k &&
+               fruits[j][0] - fruits[i][0] + min(fruits[j][0] - startPos, startPos - fruits[i][0]) <= k)
+        {
+            current_sum += fruits[j][1];
+            max_sum = max(max_sum, current_sum);
+            ++j;
+        }
+
+        while (i < fruits.size() && fruits[i][0] <= startPos)
+        {
+            current_sum -= fruits[i][1];
+            ++i;
+
+            while (j < fruits.size() &&
+                   fruits[j][0] <= startPos + k &&
+                   fruits[j][0] - fruits[i][0] + min(fruits[j][0] - startPos, startPos - fruits[i][0]) <= k)
             {
-                new_fruits[position - startPos + k] += amount;
+                current_sum += fruits[j][1];
+                max_sum = max(max_sum, current_sum);
+                ++j;
+            }
+
+            if (j == fruits.size() || fruits[j][0] > startPos + k)
+            {
+                break;
             }
         }
 
-        int current_left = 0;
-        int current_right = 0;
-        for (int i = 0; i <= k; ++i)
-        {
-            current_left += new_fruits[i];
-            current_right += new_fruits[2 * k - i];
-        }
-        int result = max(current_left, current_right);
-
-        for (int i = 1; i <= k / 3; ++i)
-        {
-            current_left -= new_fruits[2 * i - 2];
-            current_left -= new_fruits[2 * i - 1];
-            current_left += new_fruits[k + i];
-            current_right -= new_fruits[2 * k - (2 * i - 2)];
-            current_right -= new_fruits[2 * k - (2 * i - 1)];
-            current_right += new_fruits[k - i];
-            result = max(result, max(current_left, current_right));
-        }
-        return result;
+        return max_sum;
     }
 };
