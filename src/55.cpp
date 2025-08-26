b0a86842247eb101a9be77f8049484da4d47540a
ls171433 <lslslsls27@hotmail.com>
2025-08-26 09:28:08 +0800

    update 55

--- src/55.cpp
+++ src/55.cpp
@@ -3,9 +3,11 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool canJump(vector<int>& nums) {
+    bool canJump(vector<int> &nums)
+    {
         if (nums.size() == 1)
         {
             return true;
@@ -13,6 +15,7 @@ public:
         int nmax = nums[0];
         for (int i = 0; i < nmax && i < nums.size(); ++i)
         {
+            // cout << i + nums[i] << endl;
             nmax = max(nmax, i + nums[i] + 1);
             if (nmax >= nums.size())
             {
