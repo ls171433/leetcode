90d3b3beae76019105e37e9165a4160d001af4d2
ls171433 <lslslsls27@hotmail.com>
2025-09-03 12:21:38 +0800

    add 1975

---
+++ src/1975.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    long long maxMatrixSum(vector<vector<int>> &matrix)
+    {
+        long long sum = 0;
+        int negative_count = 0;
+        int zero_count = 0;
+        int min_num_abs = INT_MAX;
+        for (const auto &row : matrix)
+        {
+            for (const auto &num : row)
+            {
+                if (num > 0)
+                {
+                    sum += num;
+                    min_num_abs = min(min_num_abs, num);
+                }
+                else if (num < 0)
+                {
+                    sum += -num;
+                    ++negative_count;
+                    min_num_abs = min(min_num_abs, -num);
+                }
+                else
+                {
+                    ++zero_count;
+                }
+            }
+        }
+
+        if (zero_count > 0 || negative_count % 2 == 0)
+        {
+            return sum;
+        }
+        else
+        {
+            return sum - 2 * min_num_abs;
+        }
+    }
+};
