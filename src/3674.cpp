62ce261ad77c2873a3f3666d00566bbf6cce1952
ls171433 <lslslsls27@hotmail.com>
2025-09-07 12:09:30 +0800

    add 3674

---
+++ src/3674.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums)
+    {
+        int max_num = INT_MIN;
+        int min_num = INT_MAX;
+        for (int num : nums)
+        {
+            max_num = max(max_num, num);
+            min_num = min(min_num, num);
+        }
+        if (max_num == min_num)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+};
