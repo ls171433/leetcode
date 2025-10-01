15488bd1eeb38addcbad1bc83b8fc09def6f81dc
ls171433 <lslslsls27@hotmail.com>
2025-10-01 17:16:34 +0800

    add 1518

---
+++ src/1518.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int numWaterBottles(int numBottles, int numExchange)
+    {
+        int result = 0;
+        int emptyBottles = 0;
+        while (numBottles > 0)
+        {
+            result += numBottles;
+            emptyBottles += numBottles;
+            numBottles = emptyBottles / numExchange;
+            emptyBottles = emptyBottles % numExchange;
+        }
+        return result;
+    }
+};
