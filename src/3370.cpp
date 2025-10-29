486a4064a856e66e691bd4b9a7384bf88a71359b
ls171433 <lslslsls27@hotmail.com>
2025-10-29 11:02:09 +0800

    add 3370

---
+++ src/3370.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int smallestNumber(int n)
+    {
+        n |= n >> 1;
+        n |= n >> 2;
+        n |= n >> 4;
+        n |= n >> 8;
+        n |= n >> 16;
+        return n;
+    }
+};
